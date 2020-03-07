22c937d959be0579f6f067851d423029310f5472
John Thompson <john@springframework.guru>
2020-03-07 15:22:56 -0500

    initial add

---
+++ src/test/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.eureka.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
